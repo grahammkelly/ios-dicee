900f6631b0e5462b2705351095cf4591b9b4874d
gkelly <graham.kelly@travelportdigital.com>
2018-04-06 08:47:40 +0100

    Updated app to roll the dice based on shaking the phone:

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Page displayed. Updating dice faces")
         updateDiceFaces()
     }
 
@@ -29,6 +30,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Roll button selected. Updated dice faces")
         updateDiceFaces()
     }
 
@@ -37,18 +39,28 @@ class ViewController: UIViewController {
     }
 
     func updateDiceFaces(_ face1: Int, _ face2: Int) {
-        setImg(diceImageView1, face1)
-        setImg(diceImageView2, face2)
+        let img1 = setImg(diceImageView1, face1)
+        let img2 = setImg(diceImageView2, face2)
+        print("\tImages set to '\(imageName(diceImageView1))': '\(img1)' and '\(imageName(diceImageView2))': '\(img2)'")
     }
 
-    func setImg(_ imgView: UIImageView, _ diceTop: Int) {
-        print("Setting dice '\(imgView.accessibilityIdentifier ?? "Unknown dice")' to 'dice\(diceTop)'")
-//        imgView.image = UIImage(named: diceArray[diceTop-1])
-        imgView.image = UIImage(named: "dice\(diceTop)")
+    func imageName(_ imgView: UIImageView) -> String {
+        return imgView.accessibilityIdentifier ?? "unknown dice"
+    }
+
+    func setImg(_ imgView: UIImageView, _ diceTop: Int) -> String {
+        let imgName = diceArray[diceTop-1]
+        imgView.image = UIImage(named: imgName)
+        return imgName
     }
 
     func randomDie(sides: Int = 6) -> Int {
         return Int(arc4random_uniform(UInt32(sides))) + 1
+    }
+
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print("Phone shaken. Updating dice")
+        updateDiceFaces()
     }
 
 }
